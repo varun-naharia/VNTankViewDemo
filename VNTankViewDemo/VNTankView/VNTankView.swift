076938f6248b0e3f67be52db8ed6cf37b828317d
Varun Naharia <v.naharia@logictrixtech.com>
2017-11-28 17:56:10 +0530

    Tank color fill  done

---
+++ VNTankViewDemo/VNTankView/VNTankView.swift
@@ -0,0 +1,137 @@
+//
+//  VNTankView.swift
+//  VNTankViewDemo
+//
+//  Created by Varun Naharia on 28/11/17.
+//  Copyright © 2017 Varun Naharia. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class VNTankView: UIView {
+
+    var view: UIView!
+    @IBOutlet weak var tankView: VNTank!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setUpView()
+        xibSetup()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        self.setUpView()
+    }
+    
+    override public func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        updateView()
+    }
+    
+    func updateView() {
+            tankView.layer.borderColor = UIColor.white.cgColor
+            tankView.layer.borderWidth = 4
+        
+        
+        
+            layoutSubviews()
+    }
+    
+    func setUpView() {
+
+    }
+    
+    func xibSetup() {
+        view = loadViewFromNib()
+        view.backgroundColor = UIColor.clear
+        // use bounds not frame or it'll be offset
+        view.frame = bounds
+        view.backgroundColor = self.backgroundColor
+        // Make the view stretch with containing view
+        view.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
+        // Adding custom subview on top of our view (over any custom drawing > see note below)
+        addSubview(view)
+        self.updateView()
+    }
+    
+    func loadViewFromNib() -> UIView {
+        
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "VNTankView", bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        
+        return view
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        tankView.layer.cornerRadius = tankView.frame.width/2
+    }
+    
+    fileprivate var tankFillColor:UIColor!
+    fileprivate var fillPercentage:CGFloat = 0.0
+    
+    @IBInspectable var percent:Float
+    {
+        set
+        {
+            fillPercentage = CGFloat(newValue)
+            tankView.percentage = 1-fillPercentage
+        }
+        get
+        {
+            return Float(fillPercentage)
+        }
+        
+    }
+    
+    @IBInspectable var fillColor:UIColor
+    {
+        set{
+            tankFillColor = newValue
+            if(tankView != nil)
+            {
+                tankView.fillColor = tankFillColor
+            }
+        }
+        get{
+            return tankFillColor
+        }
+    }
+    
+//    @IBInspectable
+//    public var cornerRadius :CGFloat {
+//        set
+//        {
+//            layer.cornerRadius = newValue
+//        }
+//        get {
+//            return layer.cornerRadius
+//        }
+//    }
+    
+   
+}
+
+class VNTank: UIView {
+    var fillColor:UIColor = UIColor.blue
+    var percentage:CGFloat = 0.0
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+         //can declare as instance variable or globally which you get from web service and set it's value!!
+        let upperRect = CGRect(x: rect.origin.x, y: rect.origin.y, width: rect.size.width, height: rect.size.height*percentage)//CGRectMake(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height*percentage)
+        let lowerRect = CGRect(x: rect.origin.x, y: rect.origin.y + (rect.size.height * percentage), width: rect.size.width, height: rect.size.height*(1 - percentage))//CGRectMake(rect.origin.x, rect.origin.y + (rect.size.height * percentage), rect.size.width, rect.size.height * (1-percentage))
+        
+        UIColor.clear.set()
+        UIRectFill(upperRect)
+        fillColor.set()
+        UIRectFill(lowerRect)
+        self.layer.masksToBounds = true
+    }
+}
